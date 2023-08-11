b52a854de5d5c9b391915cc0c525b1f07cf62c1a
raogaru <raogaruinc@gmail.com>
2023-08-10 23:14:52 -0400

    reorg

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-  name = "demo-vpc"
+  name = "raogaru-vpc"
 
   cidr = "10.1.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
